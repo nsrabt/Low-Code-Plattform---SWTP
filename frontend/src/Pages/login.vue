d1a0077839aa4da89b75c0e7cdbfbc71aef72286
Luca Laurin Förger <luca.laurin.foerger@mni.thm.de>
2024-07-03 23:55:53 +0200

    fixed login

--- frontend/src/Pages/login.vue
+++ frontend/src/Pages/login.vue
@@ -177,7 +177,7 @@ export default {
         // Hier könntest du die Weiterleitung zum Dashboard implementieren
         if (user) {
           // Beispiel: Weiterleitung zur Dashboard-Seite
-          this.$router.push('/Pages/home.vue');
+          this.$router.push('/home');
         } else {
           // Handle Fehlerfallpa
           console.error('Authentication failed front');
